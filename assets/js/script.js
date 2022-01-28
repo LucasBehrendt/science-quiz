7279717edb55c7df737294a06d3f8bbda3905d2b
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-01-28 15:38:25 +0000

    Add & style menu, add function to display menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-// Get burger icon & add event listener
+// Get variables & add event listeners
 let burger = document.querySelector(".burger")
-burger.addEventListener('click', menu)
+let menu = document.querySelector(".menu")
+let home = document.querySelector(".home-section")
+burger.addEventListener('click', displayMenu)
 
 /**
  * When menu burger icon is clicked, icon will change to a cross
  * and the menu will be displayed
  */
-function menu() {
+function displayMenu() {
     burger.classList.toggle("change");
+    menu.classList.toggle("hide");
+    home.classList.toggle("hide");
 }
