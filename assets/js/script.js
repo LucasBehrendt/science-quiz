ac6a4561c80d824243da826408d566827706a899
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-01-30 13:19:16 +0000

    Add & style Difficulty page + modify showMenu function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,22 @@
 let burger = document.querySelector(".burger")
 let menu = document.querySelector(".menu")
 let home = document.querySelector(".home-section")
-burger.addEventListener('click', displayMenu)
+burger.addEventListener("click", showMenu)
 
 /**
  * When menu burger icon is clicked, icon will change to a cross
  * and the menu will be displayed
  */
-function displayMenu() {
-    burger.classList.toggle("change");
-    menu.classList.toggle("hide");
-    home.classList.toggle("hide");
+function showMenu() {
+    if (burger.classList.contains("change")) {
+        burger.classList.remove("change");
+        menu.classList.add("hide");
+        home.classList.remove("hide");
+    } else {
+        burger.classList.add("change");
+        menu.classList.remove("hide");
+        home.classList.add("hide");
+        difficultyMenu.classList.add("hide");
+        howToMenu.classList.add("hide");
+    }
 }
