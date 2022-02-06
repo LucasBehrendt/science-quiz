215dfdfffa554fc6e75ba115dff1c2d9d7fb7bc3
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-02-06 18:47:49 +0000

    Add separate js file for quiz questions & link it in index.html

---
+++ assets/js/questions.js
@@ -0,0 +1,162 @@
+// All quiz questions taken from https://www.triviawell.com/questions/science
+// Easy quiz questions
+let easyQuestions = [
+    {
+        question: "At what temperature does water first boil?",
+        options: ["-100°C", "100°C", "0°C", "1000°C"],
+        answer: "100°C"
+    },
+    {
+        question: "What is the repeated sound when sound waves bounce off other objects called?",
+        options: ["Acho", "Marco", "Echo", "Polo"],
+        answer: "Echo"
+    },
+    {
+        question: "What is the chemical symbol for water?",
+        options: ["H2O", "O2", "WHO", "H02"],
+        answer: "H2O"
+    },
+    {
+        question: "What is the tallest land animal?",
+        options: ["Giraffe", "Polar bear", "Moose", "Elephant"],
+        answer: "Giraffe"
+    },
+    {
+        question: "Which person is known for establishing the laws of motion and gravity?",
+        options: ["Stephen Hawking", "Marie Curie", "Albert Einstein", "Isaac Newton"],
+        answer: "Isaac Newton"
+    },
+    {
+        question: "What is the distance that light travels in a year called?",
+        options: ["Parsec", "Light Year", "Lumen Year", "Light Mile"],
+        answer: "Light Year"
+    },
+    {
+        question: "Which element is the primary component of steel wool?",
+        options: ["Iron", "Carbon", "Uranium", "Silver"],
+        answer: "Iron"
+    },
+    {
+        question: "What gas makes soda bubbly?",
+        options: ["Xenon", "Methane", "Nitrogen", "Carbon Dioxide"],
+        answer: "Carbon Dioxide"
+    },
+    {
+        question: "What particle in an atom has a positive charge?",
+        options: ["Neutron", "Proton", "Antitron", "Electron"],
+        answer: "Proton"
+    },
+    {
+        question: "What makes up about 70 percent of most living things?",
+        options: ["Protein", "Sugar", "Water", "Lead"],
+        answer: "Water"
+    }
+];
+
+// Medium quiz questions
+let mediumQuestions = [
+    {
+        question: "Which company manufactures Pentium chipsets?",
+        options: ["AMD", "Microsoft", "Intel", "Apple"],
+        answer: "Intel"
+    },
+    {
+        question: "What is the name for the boundary around a black hole, beyond which events cannot affect the observer?",
+        options: ["Event Horizon", "Absolute Point", "Black Line", "Event Silence"],
+        answer: "Event Horizon"
+    },
+    {
+        question: "What is table salt's chemical name?",
+        options: ["Lithium Chloride", "Potassium", "Sodium Chloride", "Sodium Hydroxide"],
+        answer: "Sodium Chloride"
+    },
+    {
+        question: "What is the wearing away of parts of the Earth by water or wind called?",
+        options: ["Corrosion", "Erosion", "Dilution", "Sedimentation"],
+        answer: "Erosion"
+    },
+    {
+        question: "What is a person who specializes in the study of plants called?",
+        options: ["Plantist", "Geologist", "Oboist", "Botanist"],
+        answer: "Botanist"
+    },
+    {
+        question: "Between which two planets does the asteroid belt lie?",
+        options: ["Uranus and Neptune", "Jupiter and Saturn", "Mars and Jupiter", "Mercury and Venus"],
+        answer: "Mars and Jupiter"
+    },
+    {
+        question: "What disaster is a combo of downdrafts and updrafts?",
+        options: ["Snowstorm", "Tsunami", "Volcanic Eruption", "Tornado"],
+        answer: "Tornado"
+    },
+    {
+        question: "What are the small bones that make up a backbone called?",
+        options: ["Sternum", "Vertebrae", "Maxilla", "Ulna"],
+        answer: "Vertebrae"
+    },
+    {
+        question: "What is the chemical symbol for gold?",
+        options: ["Ag", "Pb", "Fe", "Au"],
+        answer: "Au"
+    },
+    {
+        question: "Who was the first person in space?",
+        options: ["Yuri Gagarin", "Laika", "Neil Armstrong", "Valentina Tereshkova"],
+        answer: "Yuri Gagarin"
+    }
+];
+
+// Hard quiz questions
+let hardQuestions = [
+    {
+        question: "What is the name for the Sun's outer-most atmosphere?",
+        options: ["Stratosphere", "Corona", "Photosphere", "Chromosphere"],
+        answer: "Corona"
+    },
+    {
+        question: "How many watts is one horsepower equal to?",
+        options: ["746 W", "541 W", "121 W", "1981 W"],
+        answer: "746 W"
+    },
+    {
+        question: "The first vaccine targeted which disease?",
+        options: ["Rubella", "Smallpox", "Polio", "Measles"],
+        answer: "Smallpox"
+    },
+    {
+        question: "Heavy water contains what isotopic form of hydrogen?",
+        options: ["Deuterium", "Protium", "Hydrogen-4", "Tritium"],
+        answer: "Deuterium"
+    },
+    {
+        question: "What unit is resistance measured in?",
+        options: ["Gauss", "Ampere", "Joule", "Ohm"],
+        answer: "Ohm"
+    },
+    {
+        question: "Who is credited with creating the 'Big Bang Theory'?",
+        options: ["Albert Einstein", "Edwin Hubble", "Carl Sagan", "Georges Lemaitre"],
+        answer: "Georges Lemaitre"
+    },
+    {
+        question: "What is a scientist who specializes in the study of cells called?",
+        options: ["Eugenicist", "Embryologist", "Cytologist", "Histologist"],
+        answer: "Cytologist"
+    },
+    {
+        question: "The diameter of an atom is measured in which unit?",
+        options: ["Lambda", "Hebdometre", "Angstrom", "Basic module"],
+        answer: "Angstrom"
+    },
+    {
+        question: "In a triple bond, how many pi and sigma bonds are there?",
+        options: ["Three Pi, Zero Sigma", "Zero Pi, Three Sigma", "One Pi, Two Sigma", "Two Pi, One Sigma"],
+        answer: "Two Pi, One Sigma"
+    },
+    {
+        question: "About how many days does it take for the moon to pass through all phases?",
+        options: ["28", "29", "30", "31"],
+        answer: "29"
+    }
+];
